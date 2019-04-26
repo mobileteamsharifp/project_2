c77942842bf14e704d62eda36e7a21e425ac9ceb
mehrdad76 <mehrdadkarrabi1997@gmail.com>
2019-04-26 22:44:31 +0430

    menu created

---
+++ app/src/main/java/com/example/project_2/DeveloperDialog.java
@@ -0,0 +1,36 @@
+package com.example.project_2;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+
+import java.util.Objects;
+
+@SuppressLint("ValidFragment")
+class DeveloperDialog extends DialogFragment {
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(getContext()));
+        builder.setMessage(R.string.people_message).setTitle(R.string.people_title);
+
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.i("looooog", "clicked ok");
+            }
+        });
+
+        return builder.create();
+    }
+}
--- app/src/main/java/com/example/project_2/MainActivity.java
+++ app/src/main/java/com/example/project_2/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.project_2;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    MenuItem playMenu;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.start_menu, menu);
+        playMenu = menu.findItem(R.id.grid);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_people:
+                new DeveloperDialog().show(getSupportFragmentManager(), "");
+                return true;
+            case R.id.grid:
+                changeIcon();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    boolean flag = true;
+
+    private void changeIcon() {
+        if (playMenu != null){
+            if (flag)
+                playMenu.setIcon(R.drawable.ic_reorder_black_24dp);
+            else
+                playMenu.setIcon(R.drawable.ic_grid_on_black_24dp);
+            flag = !flag;
+        }
+    }
+
+
 }
